5928bb0e4161db836812fdc0e815780ed4386d24
Diego Henrique <diegohrp20@gmail.com>
2023-02-07 02:18:36 -0300

    Criação da classe producer que publica mensagens em um ou mais tópicos e sendo enviado os dados para o cluster kafka.

---
+++ simulador/infra/kafka/producer.go
@@ -0,0 +1,33 @@
+package kafka
+
+import (
+	"log"
+	"os"
+
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+// NewKafkaProducer cria uma instância kafka.Producer pronta para uso
+func NewKafkaProducer() *ckafka.Producer {
+	configMap := &ckafka.ConfigMap{
+		"bootstrap.servers": os.Getenv("KafkaBootstrapServers"),
+	}
+	p, err := ckafka.NewProducer(configMap)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	return p
+}
+
+// Publish é uma função simples criada para publicar uma nova mensagem no kafka
+func Publish(msg string, topic string, producer *ckafka.Producer) error {
+	message := &ckafka.Message{
+		TopicPartition: ckafka.TopicPartition{Topic: &topic, Partition: ckafka.PartitionAny},
+		Value:          []byte(msg),
+	}
+	err := producer.Produce(message, nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
